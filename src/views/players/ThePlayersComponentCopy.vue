e4213a82471628e8897cb025735f7c35138e2136
MalikSh96 <malik.sharfo@wave-crest.io>
2022-05-07 11:29:40 +0200

    small change -- wip

--- src/views/players/ThePlayersComponentCopy.vue
+++ src/views/players/ThePlayersComponentCopy.vue
@@ -19,7 +19,7 @@
                 <button type="button" 
                 class="flex items-center justify-center w-10 h-10 text-indigo-600 
                 transition-colors duration-150 rounded-full focus:shadow-outline hover:bg-indigo-100" 
-                v-if="page != 1" @click="page--"
+                v-if="page != 1" @click="page--; getPlayers(page)"
                 > 
                 <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20">
                     <path d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 
