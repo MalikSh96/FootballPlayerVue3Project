5fdbfd70e4d38cd2f424e161fd2ffa5593c74e36
MalikSh96 <malik.sharfo@wave-crest.io>
2022-05-09 13:14:21 +0200

    modal display

--- src/components/helpers/TableBackupComponent.vue
+++ src/components/helpers/TableBackupComponent.vue
@@ -1,26 +1,63 @@
 <template>
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th v-for="key in columns" :key="key" @click="sortBy(key)" :class="{ active: sortKey == key }" class="px-6 py-3 cursoer-pointer">
-                    {{ capitalize(key) }}
-                    <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
-                    </span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="entry in filteredData" :key="entry" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td v-for="key in columns" :key="key" class="px-6 py-3">
-                    {{entry[key]}}
-                </td>
-            </tr>
-        </tbody>
-  </table>
+    <div>
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th v-for="key in columns" :key="key" @click="sortBy(key)" :class="{ active: sortKey == key }" class="px-6 py-3 cursor-pointer">
+                        {{ capitalize(key) }}
+                        <span class="arrow" :class="sortOrders[key] > 0 ? 'asc' : 'dsc'">
+                        </span>
+                    </th>
+                    <th class="px-6 py-3">
+                        DETAILS
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="entry in filteredData" :key="entry" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td v-for="key in columns" :key="key" class="px-6 py-3">
+                        {{entry[key]}}
+                    </td>
+                    <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                        <button type="button" class="btn btn-primary" @click="openModal(entry.id)">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274
+                                        4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <!--modal-->
+        <transition name="fade" appear>
+            <div class="modal-overlay" v-if="modalIsVisible" @click="closeModal()"></div>
+        </transition>
+        <transition name="slide" appear>
+            <div class="modal relative bg-white rounded-lg shadow dark:bg-gray-700" v-if="modalIsVisible">
+                <PlayerModalComponent :showPlayer="showPlayer"/>
+                <button type="button"  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg 
+                text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" 
+                @click="closeModal()"
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+                </button>
+            </div>
+        </transition>
+        <!--modal-->
+    </div>
 </template>
 
 <script>
+import PlayerModalComponent from '@/components/helpers/PlayerModalComponent.vue'
+
 export default {
+    components: {
+        PlayerModalComponent
+    },
     props: {
         playersData: Object,
         filterKey: String,
@@ -28,6 +65,8 @@ export default {
     }, 
     data() {
         return {
+            modalIsVisible: false,
+            showPlayer: {},
             rows: [],
             sortKey: '',
             sortOrders: this.columns.reduce((o, key) => ((o[key] = 1), o), {}),
@@ -79,6 +118,24 @@ export default {
             .then(data => (this.club = data.item.name/*, console.log('clubs', this.club)*/))
             .catch(err => console.log(err.message))
         },
+        openModal(id) {
+            this.modalIsVisible = true
+            fetch('https://futdb.app/api/players/' + id, {
+                method: "GET",
+                headers: {
+                'content-type': 'application/json',
+                'X-AUTH-TOKEN': process.env.VUE_APP_FUT_API_KEY 
+                }
+            })
+            .then((response) => response.json())
+            .then(data => (this.showPlayer = data))
+            .catch(err => console.log(err.message))
+                    
+            console.log('after', this.showPlayer) 
+        },
+        closeModal() {
+            this.modalIsVisible = false
+        },
     },
     beforeMount () {
         console.log('CHILD playersData CHECK', this.playersData)
@@ -98,7 +155,67 @@ export default {
 </script>
 
 
-<style>
+<style scoped>
+/* MODAL */
+button.page-link {
+	display: inline-block;
+}
+button.page-link {
+    font-size: 20px;
+    color: #29b3ed;
+    font-weight: 500;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.modal-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 98;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+.modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
+
+  background-color: #FFF;
+  border-radius: 16px;
+
+  padding: 25px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform .5s;
+}
+
+.slide-enter,
+.slide-leave-to {
+  transform: translateY(-50%) translateX(100vw);
+}
+/* MODAL */
+
 th.active {
   color: #fff;
 }
